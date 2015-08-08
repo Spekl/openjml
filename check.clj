edb9fdda1b3322f9bb4935d3f6fc5eb92657c18b
John L. Singleton <jsinglet@gmail.com>
2015-08-08 11:31:26 -0400

    Creation of package

--- check.clj
+++ check.clj
@@ -2,14 +2,8 @@
  (:require [clojure.java.io :as io]
             [clojure.tools.logging :as log]))
 
-;;
-;; All checks must at least define a "default" check and can optionally define any number of named checks.
-;;
-(defcheck default
-  ;; Printing a log message...
-  (log/info  "A message")
 
-  ;; running a check
-  (log/info  "Running a check.")  
-  (run "cmd" "arg1" "${installed-package:asset.jar}" "-esc" *project-files-string* ))
+(defcheck default
+  (log/info  "The `openjml` package does not provide checks. Please install openjml-esc or openjml-rac.")
+)
 
